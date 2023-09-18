bec45f14744c5fcac01a765aa4e15ca610c7fc61
Juan Villegas <juanvj0425@gmail.com>
2023-09-17 21:46:21 -0500

    corrected logic and styled calculator

--- index.js
+++ index.js
@@ -1,33 +1,106 @@
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+let justCalculated = false;
 
+const display = document.querySelector('.display');
 
-function add(a,b) {
-    return a+b;
-}
-function substract(a,b) {
-    return a-b;
-}
-function multiply(a,b) {
-    return a*b;
-}
-function divide(a,b) {
-    return a/b;
-}
-
-function operate(a, operand, b) {
-    switch (operand) {
-        case '+':
-            return add(a,b);
-        case '-':
-            return substract(a,b);
-        case '*':
-            return multiply(a,b);
-        case '/':
-            if(b === 0) {
-                return null
-            }else {
-                return divide(a,b)
-            }
-        default:
-            return null
+function buttonClick(value){
+    if (value === '.') {
+        handleDecimal();
+    }else if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
     }
-};
+    display.innerText = buffer;
+}
+
+function handleDecimal() {
+    if (!buffer.includes('.')) {
+        buffer += '.';
+    }
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'AC':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if(previousOperator === null){
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case 'DEL':
+            if(buffer.length === 1){
+                buffer = '0';
+            }else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '-':    
+        case '*':    
+        case '/':    
+            handleMath(symbol);
+            break;
+        case '%':
+            handlePercentage();
+            break;
+    }
+}
+
+function handlePercentage() {
+    const currentValue = parseFloat(buffer);
+    buffer = (currentValue / 100).toString();
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }else if(previousOperator === '-'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '*'){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === '/'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer === '0'){
+        buffer = numberString;
+    }else{
+        buffer += numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.buttons').addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
+    })
+}
+
+init();
